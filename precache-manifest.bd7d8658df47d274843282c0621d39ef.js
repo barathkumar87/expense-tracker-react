c7e5ae62fc4d1286402a51a92be72ac02aaddc7c
Barath Kumar P S <73652035+barathkumar87@users.noreply.github.com>
2021-06-11 10:48:01 +0530

    Updates

---
+++ precache-manifest.bd7d8658df47d274843282c0621d39ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589d012b798f068c259b1b206f07d45e",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "b1bf94ee13e425a43b9e",
+    "url": "/expense-tracker-react/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expense-tracker-react/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker-react/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bf94ee13e425a43b9e",
+    "url": "/expense-tracker-react/static/js/main.1fa240bc.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
